99c6f753a7e0c558a9e51b643ce326cd90a3b9de
DESKTOP-ACTGLDB\Danila <epicycloida@yandex.ru>
2023-02-02 23:12:37 +0300

    Еще пара исправлений

--- Assets/_Source/Scripts/GameFlow/RoundController.cs
+++ Assets/_Source/Scripts/GameFlow/RoundController.cs
@@ -51,43 +51,56 @@ namespace MirrorTest.GameFlow
         }
         
 
-        private void MovePlayerTo(PlayerController playerController, Transform spawnPlace)
+        private void MovePlayerTo(PlayerController player, Transform spawnPlace)
         {
+            if(spawnPlace == null)
+                return;
+            
             _availableSpawnPlaces.Remove(spawnPlace);
-            playerController.transform.position = spawnPlace.transform.position;
+            
+            player.transform.position = spawnPlace.transform.position;
         }
 
-
+        
         private void ResetRound()
         {
-            _winnerText.text = "";
+            ResetWinner();
             _availableSpawnPlaces = _spawnPlaces;
 
             foreach (var player in _players)
                 ResetPlayer(player);
         }
-
+        
+        
+        [ClientRpc]
         private void ResetPlayer(PlayerController player)
         {
-            MovePlayerTo(player, _availableSpawnPlaces[Random.Range(0, _availableSpawnPlaces.Count - 1)]);
+            MovePlayerTo(player, _availableSpawnPlaces?[Random.Range(0, _availableSpawnPlaces.Count - 1)]);
             player.ScoreController.DropScore();
             player.ScoreController.WinScore = _winScore;
         }
         
-
+        
         private void OnWinAction(PlayerScoreController playerScore)
         {
             ShowWinner(playerScore.PlayerName);
             
             ResetRoundAsync().FireAndForget();
         }
-        
 
         [ClientRpc]
         private void ShowWinner(string name)
         {
             _winnerText.text = $"{name} WINS";
         }
+
+
+        [ClientRpc]
+        private void ResetWinner()
+        {
+            _winnerText.text = "";
+        }
+
 
         private async Task ResetRoundAsync()
         {
--- Assets/_Source/Scripts/Player/Controllers/PlayerScoreController.cs
+++ Assets/_Source/Scripts/Player/Controllers/PlayerScoreController.cs
@@ -43,7 +43,7 @@ namespace MirrorTest.Player.Controllers
             _score++;
         }
 
-
+        [Command(requiresAuthority = false)]
         public void DropScore()
         {
             _score = 0;
